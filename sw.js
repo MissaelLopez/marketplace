ce3cc88fce18762fe621405d915956c66bd6bc2e
MissaelLopez <missaelnj8@gmail.com>
2022-12-02 02:12:52 -0600

    cambio de estrategia

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/* // imports
+// imports
 importScripts("js/sw-utils.js");
 
 const STATIC_CACHE = "static-v1";
@@ -11,7 +11,7 @@ const APP_SHELL = [
   "js/app.js",
   "js/sw-utils.js",
   "pages/vender.html",
-  "pages/articulo.html",
+  "pages/articulo.html"
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -45,6 +45,7 @@ self.addEventListener("activate", (e) => {
       }
     });
   });
+  console.log(respuesta);
   e.waitUntil(respuesta);
 });
 
@@ -58,87 +59,5 @@ self.addEventListener("fetch", (e) => {
       });
     }
   });
-  e.respondWith(respuesta);
-});
- */
-
-const CACHE_STATIC_NAME = "static-v6";
-const CACHE_DYNAMIC_NAME = "dynamic-v1";
-const CACHE_INMUTABLE_NAME = "inmutable-v1";
-
-const CACHE_DYNAMIC_LIMIT = 50;
-
-function limpiarCache(cacheName, numeroItems) {
-  caches.open(cacheName).then((cache) => {
-    return cache.keys().then((keys) => {
-      if (keys.length > numeroItems) {
-        cache.delete(keys[0]).then(limpiarCache(cacheName, numeroItems));
-      }
-    });
-  });
-}
-
-self.addEventListener("install", (e) => {
-  const cacheProm = caches.open(CACHE_STATIC_NAME).then((cache) => {
-    return cache.addAll([
-      "/",
-      "index.html",
-      "js/app.js",
-      "js/sw-utils.js",
-      "pages/vender.html",
-      "pages/articulo.html",
-    ]);
-  });
-
-  const cacheInmutable = caches
-    .open(CACHE_INMUTABLE_NAME)
-    .then((cache) =>
-      cache.addAll([
-        "https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.min.js",
-        "https://cdn.tailwindcss.com",
-        "https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js",
-        "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-      ])
-    );
-
-  e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
-});
-
-self.addEventListener("activate", (e) => {
-  const respuesta = caches.keys().then((keys) => {
-    keys.forEach((key) => {
-      // static-v4
-      if (key !== CACHE_STATIC_NAME && key.includes("static")) {
-        return caches.delete(key);
-      }
-    });
-  });
-
-  e.waitUntil(respuesta);
-});
-
-self.addEventListener("fetch", (e) => {
-  // 2- Cache with Network Fallback
-  const respuesta = caches.match(e.request).then((res) => {
-    if (res) return res;
-
-    // No existe el archivo
-
-    return fetch(e.request)
-      .then((newResp) => {
-        caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
-          cache.put(e.request, newResp);
-          limpiarCache(CACHE_DYNAMIC_NAME, 50);
-        });
-
-        return newResp.clone();
-      })
-      .catch((err) => {
-        if (e.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/pages/offline.html");
-        }
-      });
-  });
-
   e.respondWith(respuesta);
 });
