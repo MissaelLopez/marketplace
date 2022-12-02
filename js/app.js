ac91cd087aaf432eba2a89afeab69e7a816e6dd4
MissaelLopez <missaelnj8@gmail.com>
2022-12-02 01:23:34 -0600

    firt commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var url = window.location.href;
+var swLocation = "/marketplace/sw.js";
+
+if (navigator.serviceWorker) {
+  if (url.includes("localhost")) {
+    swLocation = "/sw.js";
+  }
+  navigator.serviceWorker.register(swLocation);
+}
